3758a4bdade449563845fe062395de92261521d6
Zachary Kleiman <kleimanpolitics@gmail.com>
2024-07-02 22:39:56 -0400

    build win;

---
+++ build_win.bat
@@ -0,0 +1,8 @@
+@echo off
+python -m nuitka --follow-imports --standalone --enable-plugin=pyqt5 --include-package=docx --include-package=odf --include-package=PyPDF2 --windows-disable-console --output-dir=dist app.py
+echo Build complete. Executable is in the dist folder.
+pause
+
+
+
+
